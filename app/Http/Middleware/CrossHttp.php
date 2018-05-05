23a11e18cc27baade5767583de61c5db77d837e1
koyole <424188487@qq.com>
2018-05-05 12:53:46 +0800

    add cros middleware

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class CrossHttp
 {
@@ -16,10 +17,12 @@ class CrossHttp
     public function handle($request, Closure $next)
     {
         $response = $next($request);
+//        $response->header('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN']);
         $response->header('Access-Control-Allow-Origin', 'http://localhost:8080');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'true');
+
         return $response;
 
 //        return $next($request);
