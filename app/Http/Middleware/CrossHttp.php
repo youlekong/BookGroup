f07c8a6be443d42ecbedf0c80af9bfb21cc33f2b
koyole <424188487@qq.com>
2018-05-08 22:02:28 +0800

    modify cros header

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -16,8 +16,12 @@ class CrossHttp
     public function handle($request, Closure $next)
     {
         $response = $next($request);
+
+        $last_host = $_SERVER['HTTP_REFERER'];
+        preg_match("/^(http:\/\/)?([^\/]+)/i", $last_host, $matches);
+        $response->header('Access-Control-Allow-Origin', "http://{$matches[2]}"); // 允许任意域名发起的跨域请求
 //        $response->header('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN']);
-        $response->header('Access-Control-Allow-Origin', 'http://localhost:8080');
+        // $response->header('Access-Control-Allow-Origin', 'http://localhost:8080');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'true');
