fabc4923cedd464fb5808aa669702bf8836c42a2
koyole <424188487@qq.com>
2018-06-18 17:17:00 +0800

    modify

--- resources/assets/js/views/rental.vue
+++ resources/assets/js/views/rental.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <el-main>
-            <table-filter @addRecord="handleAdd" layout="exact, input, search, add, datetime"
+            <table-filter @addRecord="handleAdd" layout="exact, input, search, datetime"
                           @filter="filter" :exactTypes="exactTypes"></table-filter>
 
             <el-table :data="list" stripe style="width: 100%">
@@ -14,8 +14,8 @@
                 <el-table-column prop="end_time" label="结束时间" width="150"></el-table-column>
                 <el-table-column label="操作" width="180" fixed="right">
                     <template slot-scope="scope">
-                        <el-button size="mini"
-                                   @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                        <!--<el-button size="mini"-->
+                                   <!--@click="handleEdit(scope.$index, scope.row)">编辑</el-button>-->
                         <el-button size="mini" type="danger"
                                    @click="handleDelete(scope.$index, scope.row)">删除</el-button>
                     </template>
